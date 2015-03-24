9767daab70670edae1a7ef5cd79f3aee1db398b5
Guenter Hipler <guenter.hipler@unibas.ch>
2015-03-24 14:53:15 +0100

    first (and very basic) mechanism to store triples in Sparql

---
+++ src/main/java/org/swissbib/linked/mf/morph/functions/ItemLink.java
@@ -0,0 +1,15 @@
+package org.swissbib.linked.mf.morph.functions;
+
+import org.culturegraph.mf.morph.functions.AbstractSimpleStatelessFunction;
+
+/**
+ * Created by swissbib on 3/23/15.
+ */
+public final class ItemLink extends AbstractSimpleStatelessFunction {
+
+
+    @Override
+    protected String process(String value) {
+        return null;
+    }
+}
