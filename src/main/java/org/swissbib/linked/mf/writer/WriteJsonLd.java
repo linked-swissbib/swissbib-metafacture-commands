6f153d97ee05a8f80ae0db400d991575b7aa7617
sschuepbach <sebastian.schuepbach@unibas.ch>
2015-07-07 14:41:38 +0200

    Values sharing the same key are now in a list; context file to JSON-LD can now be added

--- src/main/java/org/swissbib/linked/mf/writer/WriteJsonLd.java
+++ src/main/java/org/swissbib/linked/mf/writer/WriteJsonLd.java
@@ -1,19 +1,10 @@
-
 package org.swissbib.linked.mf.writer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.jsonldjava.core.JsonLdError;
-import com.github.jsonldjava.core.JsonLdOptions;
-import com.github.jsonldjava.core.JsonLdProcessor;
-import com.github.jsonldjava.core.JsonLdUtils;
-import com.github.jsonldjava.utils.JsonUtils;
 import org.culturegraph.mf.framework.annotations.Description;
 import org.culturegraph.mf.framework.annotations.In;
 import org.culturegraph.mf.framework.annotations.Out;
 import org.culturegraph.mf.stream.sink.ConfigurableObjectWriter;
 import org.culturegraph.mf.util.FileCompression;
-import org.openrdf.rio.*;
-import org.openrdf.rio.rdfxml.RDFXMLParser;
 
 import java.io.*;
 import java.nio.charset.Charset;
@@ -22,23 +13,21 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 /**
- * A formatter for multiline output.
+ * An Elasticsearch Bulk API compliant output.
  *
  * @author Guenter Hipler, project swissbib, Basel
+ * @author Sebastian Schüpbach, project swissbib, Basel
  *
  */
-@Description("Writes RDF/XML documents in outpufiles. Each document is written down in one line which makes it easer for further processing")
+@Description("Outputs an Elasticsearch Bulk API compliant file.")
 @In(Object.class)
 @Out(Void.class)
 public class WriteJsonLd<T> implements ConfigurableObjectWriter<T> {
 
-
-
-
-    String header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-            "<rdf:RDF xmlns:rdau=\"http://rdaregistry.info/Elements/u/\" xmlns:void=\"http://rdfs.org/ns/void#\" xmlns:dbp=\"http://dbpedia.org/ontology/\" xmlns:dct=\"http://purl.org/dc/terms/\" xmlns:owl=\"http://www.w3.org/2004/02/skos/core#\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:skos=\"http://www.w3.org/2004/02/skos/core#\" xmlns:bibo=\"http://purl.org/ontology/bibo/\" xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\" xmlns:foaf=\"http://xmlns.com/foaf/0.1/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n<collection>\n";
-    String footer = "</collection>\n</rdf:RDF>\n";
+    String header = DEFAULT_HEADER;
+    String footer = DEFAULT_FOOTER;
     String separator = DEFAULT_SEPARATOR;
 
     boolean firstObject = true;
@@ -46,23 +35,18 @@ public class WriteJsonLd<T> implements ConfigurableObjectWriter<T> {
 
     static final String SET_COMPRESSION_ERROR = "Cannot compress Triple store";
 
-    //private String documentHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-    //                        "<rdf:RDF xmlns:rdau=\"http://rdaregistry.info/Elements/u/\" xmlns:void=\"http://rdfs.org/ns/void#\" xmlns:dbp=\"http://dbpedia.org/ontology/\" xmlns:dct=\"http://purl.org/dc/terms/\" xmlns:owl=\"http://www.w3.org/2004/02/skos/core#\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:skos=\"http://www.w3.org/2004/02/skos/core#\" xmlns:bibo=\"http://purl.org/ontology/bibo/\" xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\" xmlns:foaf=\"http://xmlns.com/foaf/0.1/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n<collection>\n";
-
-    //is set by the previous module
-    String documentHeader = null;
-
-    //private String documentFooter = "</collection>\n</rdf:RDF>";
     Pattern bibliographicResource;
     Pattern biboDoc;
+    Pattern biboDocHeader;
+    Pattern bibliographicResourceHeader;
+    Pattern bracketedValue;
 
-
+    String jsonldString;
 
     String baseOutDir = "/tmp";
     String outFilePrefix = "rdfxml1Line";
     String contextFile = "";
     int fileSize = 2000;
-    String rootTag = "rdf:RDF";
     int numberFilesPerDirectory = 300;
     int currentSubDir = 1;
     int numberOpenedFiles = 0;
@@ -71,53 +55,45 @@ public class WriteJsonLd<T> implements ConfigurableObjectWriter<T> {
     FileCompression compression = FileCompression.AUTO;
     String encoding = "UTF-8";
 
-    private RDFXMLParser rdfParser = new RDFXMLParser();
-    // private StringBuilder collector = new StringBuilder();
-
-
-
     BufferedWriter fout = null;
-
-    boolean firstWrite = true;
 
 
     public WriteJsonLd()
     {
-
-        this.bibliographicResource = Pattern.compile("<dct:BibliographicResource.*?</dct:BibliographicResource>", Pattern.MULTILINE | Pattern.DOTALL);
-        this.biboDoc = Pattern.compile("<bibo:Document.*?</bibo:Document>", Pattern.MULTILINE | Pattern.DOTALL);
-
-
+        this.bibliographicResource = Pattern.compile("(\"dct:BibliographicResource\":.*?)(})(?=})");
+        this.biboDoc = Pattern.compile("(\"bibo:Document\":.*?)(})(?=,\"index\")");
+        this.biboDocHeader = Pattern.compile("\"index\":.*?document.*?}");
+        this.bibliographicResourceHeader = Pattern.compile("\"index\":.{1,40}bibliographicResource.*?}");
+        this.bracketedValue = Pattern.compile("(\"[^\"]*\"):\\[(\"[^\"]*\")\\]");
     }
 
 
     public void setBaseOutDir (final String outDir) {
         this.baseOutDir = outDir;
-        //this.openOutFile(filename);
     }
 
-    public void setContextFile (final String contextFile) {
-        this.contextFile = contextFile;
-    }
 
     public void setOutFilePrefix (final String filePrefix) {
         this.outFilePrefix = filePrefix;
     }
 
+
     public void setFileSize (final int fileSize) {
         this.fileSize = fileSize;
     }
 
-    public void setRootTag (String rootTag) {
-        this.rootTag = rootTag;
+
+    public void setContextFile (final String contextFile) throws IOException {
+        String line = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader(contextFile))){
+            while ((line=reader.readLine()) != null) this.contextFile += line;
+        }
     }
+
 
     public void setFilesPerDir(int numberFilesPerDirectory) {
         this.numberFilesPerDirectory = numberFilesPerDirectory;
     }
-
-
-
 
 
     @Override
@@ -125,145 +101,131 @@ public class WriteJsonLd<T> implements ConfigurableObjectWriter<T> {
         return Charset.defaultCharset().toString();
     }
 
+
     @Override
     public void setEncoding(String encoding) {
         throw new UnsupportedOperationException("Cannot change encoding of Search engine");
     }
+
 
     @Override
     public FileCompression getCompression() {
         return FileCompression.NONE;
     }
 
+
     @Override
     public void setCompression(FileCompression compression) {
         throw new UnsupportedOperationException(SET_COMPRESSION_ERROR);
     }
+
 
     @Override
     public void setCompression(String compression) {
         throw new UnsupportedOperationException(SET_COMPRESSION_ERROR);
     }
 
+
     @Override
     public String getHeader() {
         return DEFAULT_HEADER;
     }
+
 
     @Override
     public void setHeader(String header) {
         this.header = header;
     }
 
+
     @Override
     public String getFooter() {
         return DEFAULT_FOOTER;
     }
+
 
     @Override
     public void setFooter(String footer) {
         this.footer = footer;
     }
 
+
     @Override
     public String getSeparator() {
         return DEFAULT_SEPARATOR;
     }
+
 
     @Override
     public void setSeparator(String separator) {
         this.separator = separator;
     }
 
+
     public void process(T obj) {
 
+        this.jsonldString = (String) obj;
+
         if (firstObject) {
-            this.documentHeader = ((String)obj).replaceAll("[\n\r]", "").trim() + "\n";
+
             this.openOutFile();
             firstObject = false;
+
         } else {
 
-            Matcher m = this.bibliographicResource.matcher((String) obj);
-            if (m.find()) {
-                this.convertToJsonLd(m.group(), "bibres");
-            }
-
-            m = this.biboDoc.matcher((String) obj);
-            if (m.find()) {
-                this.convertToJsonLd(m.group(), "bibo");
-            }
+            bracketsToList();
+            String bdh = this.convertToRootNode(this.biboDocHeader, false);
+            String bd = this.convertToRootNode(this.biboDoc, true);
+            String brh = this.convertToRootNode(this.bibliographicResourceHeader, false);
+            String br = this.convertToRootNode(this.bibliographicResource, true);
+            this.writeText(bdh + bd + brh + br);
 
         }
 
     }
 
-    private void convertToJsonLd(String body, String type) {
-        try {
-            if (this.fout != null) {
-                String input = this.documentHeader + body + "\n</rdf:RDF>";
-                InputStream is = new ByteArrayInputStream(input.getBytes());
-                // We need a sink for the Sesame transformation for json-ld can use i
-                ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-                RDFWriter rdfWriter = Rio.createWriter(RDFFormat.JSONLD, outStream);
-                this.rdfParser.setRDFHandler(rdfWriter);
-                try {
-                    rdfParser.parse(is, "http://example.com");
-                }
-                catch (RDFParseException e) {
-                    // handle unrecoverable parse error
-                }
-                catch (RDFHandlerException e) {
-                    // handle a problem encountered by the RDFHandler
-                }
-                ByteArrayInputStream inStream = new ByteArrayInputStream(outStream.toByteArray());
-                Object jsonObject = JsonUtils.fromInputStream(inStream);
-                Map context = new ObjectMapper().
-                        readValue(new File(this.contextFile), Map.class);
-                JsonLdOptions options = new JsonLdOptions();
-                Object jsonldTransformed = null;
-                LinkedHashMap jsonldTruncated = new LinkedHashMap();
-                switch (type) {
-                    case "bibres":
-                        jsonldTransformed = JsonLdProcessor.frame(jsonObject, context, options);
-                        ArrayList jsonldGraph = (ArrayList) ((LinkedHashMap) jsonldTransformed).get("@graph");
-                        for (Object temp : jsonldGraph) {
-                            Set entrySet = ((LinkedHashMap)temp).entrySet();
-                            Iterator it = entrySet.iterator();
-                            while (it.hasNext())
-                            {
-                                Map.Entry<?,?> entry = (Map.Entry<?, ?>) it.next();
-                                jsonldTruncated.put(entry.getKey(), entry.getValue());
-                            }
-                        }
-                        jsonldTruncated.put("@context", ((LinkedHashMap) jsonldTransformed).get("@context"));
-                        String idBibres = jsonldTruncated.get("@id").toString().substring(33,42);
-                        this.fout.write(genBulkHeader("testsb", idBibres, "bibliographicResource"));
-                        break;
-                    case "bibo":
-                        jsonldTruncated = (LinkedHashMap) JsonLdProcessor.compact(jsonObject, context, options);
-                        String idBibo = jsonldTruncated.get("@id").toString().substring(33,42);
-                        this.fout.write(genBulkHeader("testsb", idBibo, "document"));
-                        break;
-                }
-                JsonUtils.write(this.fout, jsonldTruncated);
-                this.fout.write("\n");
-                this.numberLinesWritten++;
-                if (this.numberLinesWritten >= this.fileSize) {
-                    this.numberLinesWritten = 0;
-                    this.closeOutFile();
-                    this.openOutFile();
-                }
+
+    private String convertToRootNode(Pattern ptr, Boolean ctx) {
+        Matcher m = ptr.matcher(this.jsonldString);
+        if (m.find()) {
+            if (ctx) {
+                return "{" + m.group(1) + "," + this.contextFile + m.group(2) + "}\n";
+            } else {
+                return "{" + m.group() + "}\n";
             }
-        } catch (IOException ioExc) {
-            System.out.println(ioExc.getMessage());
-        } catch (JsonLdError jsonLdError) {
-            jsonLdError.printStackTrace();
+        } else {
+            return null;
         }
     }
 
 
-    protected static String genBulkHeader (String index, String id, String type) {
-        return "{\"index\": {\"_index\": \"" + index + "\", \"_id\": \"" + id + "\", \"_type\": \"" + type + "\"}}\n";
+    private String bracketsToList() {
+        Map<String, List<String>> doubleKeys = new HashMap<>();
+        Matcher m1 = this.bracketedValue.matcher(this.jsonldString);
+
+        while (m1.find()) {
+            List<String> values = doubleKeys.get(m1.group(1));
+            if (values == null) {
+                values = new ArrayList<>();
+                doubleKeys.put(m1.group(1), values);
+            }
+            values.add(m1.group(2));
+        }
+        String output = "";
+        for (Map.Entry<String, List<String>> entry: doubleKeys.entrySet()) {
+            output += entry.getKey() + ":[";
+            Integer i = 0;
+            for (String element : entry.getValue()) {
+                if (i > 0) output += ",";
+                output += element;
+                i++;
+            }
+            output += "]";
+            this.jsonldString = m1.replaceFirst(output);
+        }
+        this.jsonldString = this.jsonldString.replaceAll("\"[^\"]*\":\\[\"[^\"]*\"\\],?", "");
+        this.jsonldString = this.jsonldString.replaceAll(",}", "}");
+        return this.jsonldString;
     }
 
 
@@ -284,10 +246,12 @@ public class WriteJsonLd<T> implements ConfigurableObjectWriter<T> {
         }
     }
 
+
     @Override
     public void resetStream() {
         firstObject = true;
     }
+
 
     @Override
     public void closeStream() {
@@ -302,8 +266,6 @@ public class WriteJsonLd<T> implements ConfigurableObjectWriter<T> {
 
         if (this.fout != null) {
             try {
-                // this.writeText("</" + this.rootTag + ">") ;
-                // this.writeText("]");
                 this.fout.flush();
                 this.fout.close();
             } catch (IOException ioEx) {
@@ -312,10 +274,10 @@ public class WriteJsonLd<T> implements ConfigurableObjectWriter<T> {
         }
     }
 
+
     void openOutFile () {
 
         try {
-
 
             boolean subDirexists = true;
             File subDir = new File(this.baseOutDir + File.separator + this.currentSubDir);
@@ -341,7 +303,6 @@ public class WriteJsonLd<T> implements ConfigurableObjectWriter<T> {
                 OutputStream compressor = compression.createCompressor(file, path);
 
                 this.fout = new BufferedWriter(new OutputStreamWriter(compressor,this.encoding));
-                // this.writeText(this.documentHeader);
 
             } else {
                 this.fout = null;
@@ -362,7 +323,5 @@ public class WriteJsonLd<T> implements ConfigurableObjectWriter<T> {
         }
 
     }
-
-
 
 }
