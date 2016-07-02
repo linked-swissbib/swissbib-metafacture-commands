b6c0a4939ead8e8657a9c925ae1ff27a54ca4102
Guenter Hipler <guenter.hipler@unibas.ch>
2016-07-02 17:30:56 +0200

    enhanced Author Hash

--- src/main/java/org/swissbib/linked/mf/morph/functions/AuthorHash.java
+++ src/main/java/org/swissbib/linked/mf/morph/functions/AuthorHash.java
@@ -1,11 +1,16 @@
 package org.swissbib.linked.mf.morph.functions;
 
 import org.culturegraph.mf.morph.functions.AbstractSimpleStatelessFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,19 +24,23 @@ import java.util.regex.Pattern;
  * @version 0.1
  *
  */
-public class AuthorHash extends AbstractSimpleStatelessFunction {
+public class AuthorHash extends HashGenerator {
 
 
     private final String URI_PREFIX = "http://data.swissbib.ch/agent/";
 
-    private Pattern charsToReplace = Pattern.compile(",| *",Pattern.CASE_INSENSITIVE|Pattern.MULTILINE|Pattern.DOTALL);
 
+    private static final Logger authorLogger ;
+
+    static {
+        authorLogger = LoggerFactory.getLogger(AuthorHash.class);
+    }
 
     /**
      *
      * @param value
      * we expect for value a string which contains single tokens separated by ## delimiters
-     * we don't validate the tokens if they are in a defined sequence. The single tokens are
+     * The single tokens are
      * - normalized
      * - concatenated
      * - as concatenated String used to generate a hash value
@@ -42,39 +51,101 @@ public class AuthorHash extends AbstractSimpleStatelessFunction {
     @Override
     protected String process(String value) {
 
-        String globalIdentifier = "";
         String[] valueParts =  value.split("##");
-        StringBuilder normalizedValueParts = new StringBuilder();
-        for (String valuePart : valueParts) {
-            normalizedValueParts.append(this.charsToReplace.matcher(valuePart).replaceAll(""));
-        }
-        try {
-            globalIdentifier = this.generateAuthorId(normalizedValueParts.toString());
-        } catch (URISyntaxException syntaxException) {
-            //Todo: better logging
-            syntaxException.printStackTrace();
+        String globalIdentifier = NO_HASH;
 
+        String authorType = checkTypeAndLength(valueParts);
+        if (! authorType.equalsIgnoreCase(NO_HASH)  && checkCorrectNumberOfArguments(valueParts).equalsIgnoreCase(VALUES_OK)) {
+            HashMap<String, String> mappedValues = mapValues(valueParts);
+
+            StringBuilder normalizedValueParts = new StringBuilder();
+            for (String valuePart : valueParts) {
+                normalizedValueParts.append(charsToReplace.matcher(valuePart).replaceAll(""));
+            }
+            try {
+                globalIdentifier = this.generateId(normalizedValueParts.toString());
+            } catch (URISyntaxException syntaxException) {
+                //Todo: better logging
+                syntaxException.printStackTrace();
+
+            }
         }
         return globalIdentifier;
     }
 
-
-    private String generateAuthorId(String name) throws URISyntaxException {
-        String normalizedName = null;
-        //decompose unicode characters eg. é -> e´
-        if (!Normalizer.isNormalized(name, Normalizer.Form.NFD)) {
-            normalizedName = Normalizer.normalize(name, Normalizer.Form.NFD);
+    private String checkTypeAndLength(String[] valueParts) {
+        String check = null;
+        if (!(valueParts != null && valueParts.length > 0)) {
+            check = NO_HASH;
         } else {
-            normalizedName = name;
+            String authorType = valueParts[0];
+            check = Arrays.asList("1000","1001","7000","7001").contains(authorType) ? authorType : NO_HASH;
         }
-        //remove diacritical marks
-        normalizedName = normalizedName.replaceAll( "[\\p{InCombiningDiacriticalMarks}\\p{IsLm}\\p{IsSk}]+", "" );
-        //transform to lower case characters
-        normalizedName = normalizedName.toLowerCase();
-        //URL generation
-        UUID uuid = UUID.nameUUIDFromBytes(normalizedName.getBytes(Charset.forName("UTF-8")));
-        //return new URI(this.URI_PREFIX + uuid.toString());
-        return uuid.toString();
+
+        return check;
     }
+
+    private String checkCorrectNumberOfArguments(String[] values) {
+
+        String check = NO_HASH;
+        switch (values[0]) {
+            case "1001":
+            case "7001":
+                check = values.length == 10 ? VALUES_OK : NO_HASH;
+                break;
+            case "1000":
+            case "7000":
+                check = values.length == 9 ? VALUES_OK : NO_HASH;
+                break;
+        }
+
+        return check;
+    }
+
+    private HashMap<String,String> mapValues(String [] values) {
+
+        HashMap<String, String> mappedValues = new HashMap<>();
+
+        String authorType = values[0];
+        switch (authorType) {
+            case "1001":
+            case "7001":
+                mappedValues.put("typ",values [0]);
+                mappedValues.put("swissbib_id",values [1]);
+                mappedValues.put("number",values [2]);
+                mappedValues.put("lastname",values [3]);
+                mappedValues.put("firstname",values [4]);
+                mappedValues.put("title",values [5]);
+                mappedValues.put("lifedata",values [6]);
+                mappedValues.put("fullname",values [7]);
+                mappedValues.put("publishYear",values [8]);
+                mappedValues.put("title245a",values [9]);
+
+                break;
+            case "1000":
+            case "7000":
+                mappedValues.put("typ",values [0]);
+                mappedValues.put("swissbib_id",values [1]);
+                mappedValues.put("number",values [2]);
+                mappedValues.put("name",values [3]);
+                mappedValues.put("title",values [4]);
+                mappedValues.put("lifedata",values [5]);
+                mappedValues.put("fullname",values [6]);
+                mappedValues.put("publishYear",values [7]);
+                mappedValues.put("title245a",values [8]);
+                break;
+        }
+
+
+        return mappedValues;
+
+    }
+
+    private String pickUpValuesForHashKey(HashMap<String, String> mappedValues) {
+        String stringForHashId = "";
+        return stringForHashId;
+    }
+
+
 
 }
---
+++ src/main/java/org/swissbib/linked/mf/morph/functions/HashGenerator.java
@@ -0,0 +1,41 @@
+package org.swissbib.linked.mf.morph.functions;
+
+import org.culturegraph.mf.morph.functions.AbstractSimpleStatelessFunction;
+
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.text.Normalizer;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+/**
+ * Created by swissbib on 6/30/16.
+ */
+abstract class HashGenerator extends AbstractSimpleStatelessFunction {
+
+    static final String NO_HASH = "NO_HASH";
+    static final String VALUES_OK = "OK";
+
+    static final Pattern charsToReplace = Pattern.compile(",| *",Pattern.CASE_INSENSITIVE|Pattern.MULTILINE|Pattern.DOTALL);
+
+
+    String generateId(String name) throws URISyntaxException {
+        String normalizedName = null;
+        //decompose unicode characters eg. é -> e´
+        if (!Normalizer.isNormalized(name, Normalizer.Form.NFD)) {
+            normalizedName = Normalizer.normalize(name, Normalizer.Form.NFD);
+        } else {
+            normalizedName = name;
+        }
+        //remove diacritical marks
+        normalizedName = normalizedName.replaceAll( "[\\p{InCombiningDiacriticalMarks}\\p{IsLm}\\p{IsSk}]+", "" );
+        //transform to lower case characters
+        normalizedName = normalizedName.toLowerCase();
+        //URL generation
+        UUID uuid = UUID.nameUUIDFromBytes(normalizedName.getBytes(Charset.forName("UTF-8")));
+        //return new URI(this.URI_PREFIX + uuid.toString());
+        return uuid.toString();
+    }
+
+
+}
